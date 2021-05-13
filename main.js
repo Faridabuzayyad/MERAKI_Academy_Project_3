fb35f16600f9c65f662aeb40f2264fd3b3505cfe
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-14 00:42:19 +0300

    Starter Code (Express) - Done

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });
