735ffd9b24f4a4d9fa561784c3277e4da0da8916
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-14 23:47:37 +0300

    Ticket #1 - getAllArticles - Done

--- main.js
+++ main.js
@@ -2,7 +2,39 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
 
+//Ticket #1
+const articles = [
+    {
+    id: 1,
+    title: 'How I learn coding?',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    {
+    id: 2,
+    title: 'Coding Best Practices',
+    description:
+    'Lorem, ipsum dolor sit, Quam, mollitia.',
+    author: 'Besslan',
+    },
+    {
+    id: 3,
+    title: 'Debugging',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    ];
+
+const getAllArticles = (req , res , next)=>{
+    res.status(200);
+    res.json(articles);
+}
+
+app.get('/articles' , getAllArticles);
 
 
 
@@ -17,5 +49,5 @@ const PORT = 5000;
 
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+    console.log(`Project-3 app listening at http://localhost:${PORT}`);
   });
