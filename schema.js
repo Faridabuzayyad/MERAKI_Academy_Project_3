c817f7e1008c14fbc119151353ced7e85e0dd9bd
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-21 01:00:21 +0300

    3.B #2. Update users schema - Done

--- schema.js
+++ schema.js
@@ -8,6 +8,7 @@ const users = new mongoose.Schema({
     country: String,
     email: {type :String, unique: true},
     password: String,
+    role: {type: mongoose.Schema.ObjectId, ref:"Role"},
 });
 //Ticket 3.A #1 
 users.pre("save",async function(){
@@ -35,7 +36,7 @@ const roles = new mongoose.Schema({
     permissions:['MANAGE_USERS', 'CREATE_COMMENTS']
 })
 
-const role = mongoose.model("ROLE", roles);
+const role = mongoose.model("Role", roles);
 const user = mongoose.model("User", users);
 const article = mongoose.model("Article", articles);
 const comment = mongoose.model("Comment", comments);
